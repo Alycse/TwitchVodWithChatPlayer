08e29395dbe2bef1363f523baa8852953155254b
Joshua Barcena <Alycse@gmail.com>
2019-07-07 11:48:11 +0800

    Modified forward/backward time change

--- TwitchVodPlayer/Video/Constants.cs
+++ TwitchVodPlayer/Video/Constants.cs
@@ -1,8 +1,8 @@
 ﻿namespace TwitchVodPlayer.Video {
     class Constants {
 
-        public static readonly long ForwardMilliseconds = 5000;
-        public static readonly long BackwardMilliseconds = -5000;
+        public static readonly double ForwardMilliseconds = 5.0;
+        public static readonly double BackwardMilliseconds = -5.0;
 
     }
 }
